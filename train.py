8b77f9845979bf3df24604e8e8a3a2e0534780d1
ZZW <62459108+zwzheng45@users.noreply.github.com>
2023-01-27 10:25:10 +0000

    自动将训练后模型保存到云端，仅保留1个checkpoint

--- train.py
+++ train.py
@@ -219,9 +219,9 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
             if global_step % hps.train.eval_interval == 0:
                 evaluate(hps, net_g, eval_loader, writer_eval)
                 utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch,
-                                      os.path.join(hps.model_dir, "G_{}.pth".format(global_step)))
+                                      os.path.join(hps.model_dir, "G.pth".format(global_step)))
                 utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch,
-                                      os.path.join(hps.model_dir, "D_{}.pth".format(global_step)))
+                                      os.path.join(hps.model_dir, "D.pth".format(global_step)))
         global_step += 1
 
     if rank == 0:
