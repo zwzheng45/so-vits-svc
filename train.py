fa792d736729ee7806ef5d2664e6528693d22895
ZZW <62459108+zwzheng45@users.noreply.github.com>
2023-02-01 20:34:27 +0000

    从上一个checkpoint读取

--- train.py
+++ train.py
@@ -89,9 +89,9 @@ def run(rank, n_gpus, hps):
     net_d = DDP(net_d, device_ids=[rank])
 
     try:
-        _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "G_*.pth"), net_g,
+        _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "G.pth"), net_g,
                                                    optim_g)
-        _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "D_*.pth"), net_d,
+        _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "D.pth"), net_d,
                                                    optim_d)
         global_step = (epoch_str - 1) * len(train_loader)
     except:
