34fb22010dba15ba39ba117ae517bb5df0b3a172
innnky <xiyiji508@gmail.com>
2023-02-20 14:13:18 +0800

    fix

--- train.py
+++ train.py
@@ -95,7 +95,8 @@ def run(rank, n_gpus, hps):
                                                    optim_g, skip_optimizer)
         _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "D_*.pth"), net_d,
                                                    optim_d, skip_optimizer)
-        global_step = max((epoch_str - 1) * len(train_loader), 0)
+        epoch_str = max(epoch_str, 1)
+        global_step = (epoch_str - 1) * len(train_loader)
     except:
         print("load old checkpoint failed...")
         epoch_str = 1
